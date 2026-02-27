43c1ab4f6af24463611af057c1a6ebe68e2ff5d5
Rayonalia <rayonalialabs@gmail.com>
2026-02-27 17:19:22 +0200

    Optimize scroll handling with requestAnimationFrame

--- scripts.js
+++ scripts.js
@@ -1,20 +1,24 @@
 window.onload = function () {
+
     window.scrollTo(0, 0); 
+
     handleScroll(); 
     handleSectionScroll();
 };
 
 window.addEventListener("scroll", function () {
-    handleScroll(); 
-    handleSectionScroll(); 
+
+    requestAnimationFrame(() => {
+        handleScroll(); 
+        handleSectionScroll(); 
+    });
 });
 
 function handleScroll() {
     const logoName = document.querySelector("header strong.logo-name");
     const logo = document.querySelector(".logo");
-    const scrollPosition = window.scrollY;
-
-    if (scrollPosition > 0) {
+    
+    if (window.scrollY > 0) {
         logo.classList.add("visible-logo");
         logoName.classList.remove("visible-logo-name"); 
     } else {
@@ -22,33 +26,58 @@ function handleScroll() {
         logoName.classList.add("visible-logo-name");
     }
 }
-
+ 
 function handleSectionScroll() {
     const sections = document.querySelectorAll('.section');
     const windowHeight = window.innerHeight;
+    const triggerPoint = windowHeight * 0.80;
 
     sections.forEach((section) => {
         const rect = section.getBoundingClientRect();
         
-        const triggerPoint = windowHeight * 0.80; 
-
         if (rect.top < triggerPoint && rect.bottom > 0) {
-            
             const progress = (triggerPoint - rect.top) / triggerPoint;
             
             const opacityValue = Math.min(1, progress * 5); 
-            
+
             const translateYValue = Math.max(0, 50 * (1 - progress)); 
 
             section.style.transform = `translateY(${translateYValue}px)`;
             section.style.opacity = opacityValue;
 
-        } else if (rect.top >= triggerPoint) {
+        } 
+
+        else if (rect.top >= triggerPoint) {
             section.style.transform = 'translateY(50px)';
             section.style.opacity = 0;
-        } else if (rect.bottom <= 0) {
+        } 
+
+        else if (rect.bottom <= 0) {
              section.style.transform = 'translateY(0)';
              section.style.opacity = 1;
+        }
+    });
+}
+
+const uploadBtn = document.querySelector('.upload-btn');
+const fileList = document.getElementById('file-list');
+
+if (uploadBtn) {
+    uploadBtn.addEventListener('click', () => {
+        const fileName = prompt("Nom du fichier à simuler :");
+        
+        if (fileName) {
+            const fileDiv = document.createElement('div');
+            fileDiv.className = 'file-card';
+
+            fileDiv.innerHTML = `
+                <span>📄</span> 
+                <p>${fileName}</p>
+            `;
+            
+            fileList.appendChild(fileDiv);
+            
+            alert(`${fileName} a été crypté et stocké en toute sécurité sur le territoire national (RDC) !`);
         }
     });
 }
